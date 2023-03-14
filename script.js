67faf5e9036398fae7eef45b47d72d68ba21efa2
OscarV <oovillag@gmail.com>
2023-03-14 15:31:16 -0500

    Add script.js file

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".menu");
+
+    hamburger.addEventListener ("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active")
+    })
+    
