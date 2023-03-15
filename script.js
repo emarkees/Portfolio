a1cc9f49c75ec08f4a5a14bf5c727074e93ef349
emarkees <rphillipse@gmail.com>
2023-03-15 02:01:00 -0600

    Update linters errors script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger ');
 const navMenu = document.querySelector('.menu');
 
-hamburger.addEventListener ('click', () => {
+hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
