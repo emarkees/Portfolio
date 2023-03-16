5f508d07694d1bff14a0a045c0002b1cb02e4097
emarkees <rphillipse@gmail.com>
2023-03-16 03:33:15 -0600

    Update eslint error Script.js file

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ hamburger.addEventListener('click', () => {
 });
 
 document.querySelectorAll('.menuaction')
-.forEach(n => n.addEventListener('click', () => {
-   hamburger.classList.remove('active');
-   navMenu.classList.remove('active');
-}));
+  .forEach((n) => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));
