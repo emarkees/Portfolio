f00d83ed8afeb053bb9665121dee44510bbb5417
emarkees <rphillipse@gmail.com>
2023-03-16 02:28:55 -0600

    Update script.js as requested by the PR

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const hamburger = document.querySelector('.hamburger ');
-const navMenu = document.querySelector('.menu');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".menuaction")
+.forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
