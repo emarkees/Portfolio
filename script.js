9c4335921b3981adc3b167f9f581be3b84ccc806
emarkees <rphillipse@gmail.com>
2023-03-16 02:34:26 -0600

    Update Script.js errors

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
-document.querySelectorAll(".menuaction")
-.forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
+document.querySelectorAll('.menuaction')
+.forEach(n => n.addEventListener('click', () => {
+   hamburger.classList.remove('active');
+   navMenu.classList.remove('active');
 }));
