644254b8f8e90a0ad60dd29fdee5d1be2f5386d5
emarkees <rphillipse@gmail.com>
2023-03-17 08:30:51 -0600

    Update modal styledes.css file

--- script.js
+++ script.js
@@ -25,17 +25,17 @@ document.querySelectorAll('.menuaction')
   const closehamburger = document.querySelector('.closehamburger');
   const worksSection = document.querySelector('.work-container');
   
-  const data = [
-    {
-      name: 'Tonic',
-      shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi architecto 
-      mollitia, earum fugiat possimus expedita! Qui nostrum blanditiis est, cupiditate autem 
+const data = [
+  {
+    name: 'Tonic',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi architecto 
+     mollitia, earum fugiat possimus expedita! Qui nostrum blanditiis est, cupiditate autem 
       fugiat non consequatur consectetur itaque dolor, quo deserunt sequi.
       Consequuntur placeat, delectus eius nemo veritatis eaque quas. Nulla minima, harum nobis 
       odio, in non placeat voluptas provident vitae, quaerat maxime minus amet. Libero nisi 
       maiores molestias repudiandae natus provident.`,
-      featuredImage: './asset/images/avocado.svg',
+    featuredImage: './asset/images/avocado.svg',
       technologies: ['html', 'css', 'javaScript'],
       linkToLive: 'https://alexisjw.github.io/set-up-myFirst-mobile/',
       linkToSource: 'https://github.com/AlexisJW/set-up-myFirst-mobile',
@@ -65,30 +65,30 @@ document.querySelectorAll('.menuaction')
       Consequuntur placeat, delectus eius nemo veritatis eaque quas. Nulla minima, harum nobis 
       odio, in non placeat voluptas provident vitae, quaerat maxime minus amet. Libero nisi 
       maiores molestias repudiandae natus provident.`,
-      featuredImage: './asset/images/yoga.svg',
-      technologies: ['html', 'css', 'javaScript'],
-      linkToLive: 'https://alexisjw.github.io/set-up-myFirst-mobile/',
-      linkToSource: 'https://github.com/AlexisJW/set-up-myFirst-mobile',
-      cardNo: '#third-card',
+    featuredImage: './asset/images/yoga.svg',
+  technologies: ['html', 'css', 'javaScript'],
+  linkToLive: 'https://alexisjw.github.io/set-up-myFirst-mobile/',
+  linkToSource: 'https://github.com/AlexisJW/set-up-myFirst-mobile',
+  cardNo: '#third-card',
     },
     {
-      name: 'Multi-Post Stories',
-      shortDescription: ' daily selection of privately personalized reads; no accounts or sign-ups required.',
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  name: 'Multi-Post Stories',
+  shortDescription: ' daily selection of privately personalized reads; no accounts or sign-ups required.',
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
 
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
-      featuredImage: './asset/images/professional.svg',
-      technologies: ['html', 'css', 'javaScript'],
-      linkToLive: 'https://alexisjw.github.io/set-up-myFirst-mobile/',
-      linkToSource: 'https://github.com/AlexisJW/set-up-myFirst-mobile',
-      cardNo: '#fourth-card',
-    },
+   featuredImage: './asset/images/professional.svg',
+   technologies: ['html', 'css', 'javaScript'],
+    linkToLive: 'https://alexisjw.github.io/set-up-myFirst-mobile/',
+    linkToSource: 'https://github.com/AlexisJW/set-up-myFirst-mobile',
+    cardNo: '#fourth-card',
+  },
   ];
-  
-  let html = '';
 
-  data.forEach((item) => {
-    html += `
+let html = '';
+
+data.forEach((item) => {
+  html += `
     <div class="card-section" id="${item.cardNo}">
       <div class="card-section-background-image">
         <img src="${item.featuredImage}" alt="Avocado" class="card-background-image">
@@ -102,42 +102,41 @@ document.querySelectorAll('.menuaction')
             <li><h1 class="role">Back End Dev</h1></li>
             <span class="span"></span>
             <li><h1 class="year">2015</h1></li>
-          </ul>             
+          </ul>
         </div>
         <p class="card-primary-body-text">${item.shortDescription}</p> 
         <ul class="card-primary-tag  tag-font">
         ${item.technologies.map((tech) => `<li class='  modal-tag tag-font' > ${tech}</li>`) .join('')
-       }
+}
         </ul>
         <div id="action">
           <button type="button" class="normalButtn"> See project</button>
         </div>
       </div>
     </div>
-    `
-  });
+    `;
+});
 
-  worksSection.innerHTML = html;
+worksSection.innerHTML = html;
 
-  const projectButton = document.querySelectorAll('.normalButtn');
+const projectButton = document.querySelectorAll('.normalButtn');
 
-  projectButton.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      const modal = document.querySelector('.modal');
-      const title = modal.querySelector('.primary-title');
-      const img = modal.querySelector('img.card-background-image');
-      const details = modal.querySelector('.modal-primary-body-text ');
-      const technologie = modal.querySelector('.modal-tag');
-      const linkLiveDemo = modal.querySelector('a.link-live');
-      const linkGit = modal.querySelector('a.link-git');
-  
-      title.textContent = data[index].name;
-      img.src = data[index].featuredImage;
-      details.textContent = data[index].description;
-      technologie.innerHTML = data[index].technologies.map((item) => `<li>${item}</li>`).join('');
-      linkLiveDemo.href = data[index].linkToLive;
-      linkGit.href = data[index].linkToSource;
-  
-      modal.style.display = 'block';
+projectButton.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    const title = modal.querySelector('.primary-title');
+    const img = modal.querySelector('img.card-background-image');
+    const details = modal.querySelector('.modal-primary-body-text ');
+    const technologie = modal.querySelector('.modal-tag');
+    const linkLiveDemo = modal.querySelector('a.link-live');
+    const linkGit = modal.querySelector('a.link-git');
+    title.textContent = data[index].name;
+    img.src = data[index].featuredImage;
+    details.textContent = data[index].description;
+    technologie.innerHTML = data[index].technologies.map((item) => `<li>${item}</li>`).join('');
+    linkLiveDemo.href = data[index].linkToLive;
+    linkGit.href = data[index].linkToSource;
+
+    modal.style.display = 'block';
     });
   });
